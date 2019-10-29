2497dc3681e2632d750ef0ddc9c4c21f35e4de77
AlexJBz <alexjkenworthy@gmail.com>
2019-10-29 10:34:26 +0000

    Created canvas class, tested that it works

--- js/framework.js
+++ js/framework.js
@@ -1,1 +1,8 @@
 // This is a very bespoke and probably quite bad canvas framework
+class Canvas {
+    constructor(width, height) {
+        this.view = document.createElement('canvas');
+        this.view.width = width;
+        this.view.height = height;
+    }
+}
---
+++ js/main.js
@@ -0,0 +1,2 @@
+canvas = new Canvas(800, 500);
+document.body.appendChild(canvas.view);
