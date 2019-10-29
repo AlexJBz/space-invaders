c4f9f45c0565d0a711999d9bdcfd3753c7191370
AlexJBz <alexjkenworthy@gmail.com>
2019-10-29 12:07:52 +0000

    Beginnings of a framework, rectangles can now be added and manipulated

--- js/framework.js
+++ js/framework.js
@@ -2,7 +2,54 @@
 class Canvas {
     constructor(width, height) {
         this.view = document.createElement('canvas');
+        this.context = this.view.getContext('2d');
         this.view.width = width;
         this.view.height = height;
+        this.graphics = [];
+        this.renderer = setInterval(()=> {
+            this.render();
+        }, 1000 / 30);
+    }
+
+    render() {  
+        this.graphics.forEach(graphic => {
+            graphic.draw(this.context);
+        });
+    }
+
+    addChild(child) {
+        this.graphics.push(child);
     }
 }
+
+class Graphic {
+    constructor(x, y, width, height) {
+        this.position = {
+            x: x,
+            y: y,
+            set (x, y) {
+                this.x = x;
+                this.y = y;
+            }
+        }
+        this.width = width;
+        this.height = height;
+    }
+
+    draw () {
+        console.log('[FRAMEWORK ERROR]: SET A DRAW FUNCTION FOR GRAPHIC');
+    }
+}
+
+class Rectangle extends Graphic {
+    constructor(x, y, width, height) {
+        super(x, y, width, height);
+    }
+
+    draw (context) {
+        context.beginPath();
+        context.rect(this.position.x, this.position.y, this.width, this.height);
+        context.stroke();
+    }
+}
+
--- js/main.js
+++ js/main.js
@@ -1,2 +1,11 @@
 canvas = new Canvas(800, 500);
 document.body.appendChild(canvas.view);
+
+let rectangle = new Rectangle(20, 20, 100, 150);
+let rectangle2 = new Rectangle(50, 50, 100, 100);
+
+canvas.addChild(rectangle);
+canvas.addChild(rectangle2);
+
+rectangle.position.set(300, 300);
+rectangle2.position.set(100, 100)
