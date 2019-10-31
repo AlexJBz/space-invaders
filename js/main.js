e3680418a65abb1809ce8fa9ea4ffc17a5ae4d0d
AlexJBz <alexjkenworthy@gmail.com>
2019-10-31 11:03:38 +0000

    Can move player ship, logic for shooting implemented

--- js/main.js
+++ js/main.js
@@ -1,17 +1,75 @@
-canvas = new Canvas(800, 500);
-document.body.appendChild(canvas.view);
 
-// new Sprite(x coordinate, y coordinate, width, height)
-// new Rectangle(x coordinate, y coordinate, width, height, colour ('#HEX'), fill (true/false), outline (true/false), thickness, outline colour)
-// once you've made a new sprite/rectangle then make sure to add it to the canvas
-// canvas.addGraphic(newGraphic);
 
-// Refer to the below, don't mess with ANY of framework.js
+// let enemyTest = new Sprite(0, 0, 40, 36, '../img/enemy.png');
+// let playerTest = new Sprite(50, 0, 52, 36, '../img/player.png');
 
-let enemyTest = new Sprite(0, 0, 40, 36, '../img/enemy.png');
-let playerTest = new Sprite(50, 0, 52, 36, '../img/player.png');
+// canvas.addGraphic(enemyTest);
+// canvas.addGraphic(playerTest);
 
-canvas.addGraphic(enemyTest);
-canvas.addGraphic(playerTest);
+// playerTest.position.set(250, 250);
 
-playerTest.position.set(250, 250);
+let game = {
+    settings: {
+        moveSpeed: 5,
+    },
+    canvas: null,
+    playerClass: class Player {
+        constructor() {
+            this.health = 100;
+            this.ship = new Sprite(0, 0, 52, 36, '../img/player.png');
+            game.canvas.addGraphic(this.ship);
+            this.ship.position.set((game.canvas.view.width / 2 - this.ship.width / 2), (game.canvas.view.height - this.ship.height - 10));
+            this.controlListener();
+            this.moveKey = null;
+            this.moveInterval = null;
+        }
+
+        controlListener() {
+            // 97 A, 100 D, 32 SPACE
+            window.addEventListener('keypress', key => {
+                if (key.keyCode == 97 || key.keyCode == 100) {
+                    if (!this.moveInterval) {
+                        this.moveKey = key.keyCode;
+                        this.moveInterval = setInterval(() => { this.move(key.keyCode) }, 1000 / 30);
+                    }
+                }
+            });
+            window.addEventListener('keyup', key => {
+                if (key.keyCode + 32 == this.moveKey) {
+                    clearInterval(this.moveInterval);
+                    this.moveInterval = null;
+                } else if (key.keyCode == 32) {
+                    this.shoot();
+                }
+            });
+        }
+
+        shoot () {
+            console.log('Shoot god dammit!');
+        }
+
+        move(keyCode) {
+            if (keyCode == 97) {
+                if (this.ship.position.x - game.settings.moveSpeed <= 10) {
+                    this.ship.position.x = 10;
+                } else {
+                    this.ship.position.x -= game.settings.moveSpeed;
+                }
+            } else {
+                if (this.ship.position.x + game.settings.moveSpeed >= game.canvas.view.width - this.ship.width - 10) {
+                    this.ship.position.x = game.canvas.view.width - this.ship.width - 10;
+                } else {
+                    this.ship.position.x += game.settings.moveSpeed;
+                }
+            }
+        }
+    },
+    player: null,
+    init () {
+        this.canvas = new Canvas(800, 500);
+        document.body.appendChild(this.canvas.view);
+        this.player = new this.playerClass();
+    }
+}
+
+game.init();
